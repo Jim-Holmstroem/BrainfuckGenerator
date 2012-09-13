26f22a8c701e183bd128f910767d9c213fc0b239
Jim Holmström <jim.holmstroem@gmail.com>
2012-09-13 21:47:16 +0200

    started on crossover

--- src/genetic_programming.py
+++ src/genetic_programming.py
@@ -1,1 +1,23 @@
 from __future__ import print_function
+import numpy as np
+
+
+#Genetic operatorions
+def crossover(program_a, program_b, ncrossovers = 2):
+    """
+    
+    returns a crossover and it's complement
+    """
+
+    crossovers_a = np.shuffle(len(program_a))[:ncrossovers].sort() #TODO chance to np.choice instead
+    crossovers_b = np.shuffle(len(program_b))[:ncrossovers].sort() #TODO chance to np.choice instead
+    #NOTE different crossoverpoints makes it possible to change length even without the mutations.
+
+    pass
+
+def mutate(program_a):
+    """
+    returns a mutation of program_a
+    """
+    pass
+
