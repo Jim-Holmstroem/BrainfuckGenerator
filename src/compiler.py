e6172304dec8bd5585ed38a0a843cd7df2ed1928
Jim Holmström <jim.holmstroem@gmail.com>
2012-09-13 23:58:38 +0200

    started on the parser

--- src/compiler.py
+++ src/compiler.py
@@ -6,13 +6,29 @@ def compile(program):
     program - a raw text program
 
     Is getting parsed with the ebnf-syntax
-    program = { program | loop } | code
-    loop = { "[", program, "]" }
+    program = ( code | loop ), [ program ] #tailrecursion with either code or a loopbody
+    loop = "[", [ program ], "]" #empty loop is just silly.
     code = { "+" | "-" | ">" | "<" | "." | "," }
-
 
     returns compiled program.program
     """
     
+    def get_code(program):
+        pass
+    def is_code(program):
+        pass
+    def get_loop(program):
+        pass
+    def is_loop(program):
+        pass
+    def is_loop_end(program):
+        pass
+    def get_program(program):
+        pass
+    def is_program(program):
+        pass
+
+     
     
-    
+
+    return prgm.program(data)
