83b0354715ebdde2ddc9e50fb0fc7a22b1636fe0
Jim Holmström <jim.holmstroem@gmail.com>
2012-09-16 20:01:45 +0200

    Made the compiler work, it's kinda ugly at the moment, later fix

--- src/compiler.py
+++ src/compiler.py
@@ -1,5 +1,10 @@
 from __future__ import print_function
+import itertools as it
+import numpy as np
+import operator as op
+
 import program as prgm 
+import utils as utils
 
 def compile(program):
     """
@@ -12,7 +17,47 @@ def compile(program):
 
     returns compiled program.program
     """
-    pos = 0    
+    print("program:", program)
+    bracket_levels = utils.bracket_levels( program )
+    outer_start = np.where(
+        (np.array(bracket_levels) == [ 1, 1]).all(axis=1)
+    )[0]
+    outer_end = np.where(
+        (np.array(bracket_levels) == [-1, 1]).all(axis=1)
+    )[0]
+    if(len(outer_start)==0 or len(outer_end)==0): #basecase
+        return prgm.program(program)
+    print("outer_start:", outer_start)
+    print("outer_end:", outer_end)
+    print("zipped:", 
+        zip(
+            np.hstack([[0,], outer_end]), 
+            np.hstack([outer_start, [len(program),]])
+        )
+    )
+    marks = reduce(op.add, 
+        zip(
+            np.hstack([[-1,], outer_end]), 
+            np.hstack([outer_start, [len(program),]])
+        )
+    ) #logically works like ]code[loop]code[
+    print("marks:", marks)
+    spans = zip(
+        np.array(marks)+1,
+        np.array(marks[1:])
+    )
+    print("spans:", spans)
+    code_spans = spans[::2]
+    loop_spans = spans[1::2]
+    code_parts = map(lambda (start, end): prgm.program(program[start:end]), code_spans)
+    print("code_spans:", code_spans)
+    print("code_parts:",map(str, code_parts))
+    loop_parts = map(lambda (start, end): prgm.loop(program[start:end]),loop_spans)
+    print("loop_spans:", loop_spans)
+    print("loop_parts:",map(str, loop_parts))
+    return prgm.program(reduce(op.add, it.izip_longest(code_parts, loop_parts, fillvalue=''))) 
 
+print(str(compile('+++--->>>.....<')))
+print(str(compile('+++---[>.<]...<')))
+print(str(compile('+++[-+.][>+.]>....<')))
 
-    return prgm.program(data)
