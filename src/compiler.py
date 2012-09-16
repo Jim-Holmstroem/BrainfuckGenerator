344c8180a482db3fae6dd4a0f8b2ec006472f39f
Jim Holmström <jim.holmstroem@gmail.com>
2012-09-16 20:16:07 +0200

    added the tests a little bit nicer

--- src/compiler.py
+++ src/compiler.py
@@ -17,7 +17,7 @@ def compile(program):
 
     returns compiled program.program
     """
-    print("program:", program)
+    #print("program:", program)
     bracket_levels = utils.bracket_levels( program )
     outer_start = np.where(
         (np.array(bracket_levels) == [ 1, 1]).all(axis=1)
@@ -27,37 +27,41 @@ def compile(program):
     )[0]
     if(len(outer_start)==0 or len(outer_end)==0): #basecase
         return prgm.program(program)
-    print("outer_start:", outer_start)
-    print("outer_end:", outer_end)
-    print("zipped:", 
-        zip(
-            np.hstack([[0,], outer_end]), 
-            np.hstack([outer_start, [len(program),]])
-        )
-    )
+    #print("outer_start:", outer_start)
+    #print("outer_end:", outer_end)
     marks = reduce(op.add, 
         zip(
             np.hstack([[-1,], outer_end]), 
             np.hstack([outer_start, [len(program),]])
         )
     ) #logically works like ]code[loop]code[
-    print("marks:", marks)
+    #print("marks:", marks)
     spans = zip(
         np.array(marks)+1,
         np.array(marks[1:])
     )
-    print("spans:", spans)
+    #print("spans:", spans)
     code_spans = spans[::2]
     loop_spans = spans[1::2]
     code_parts = map(lambda (start, end): prgm.program(program[start:end]), code_spans)
-    print("code_spans:", code_spans)
-    print("code_parts:",map(str, code_parts))
+    #print("code_spans:", code_spans)
+    #print("code_parts:",map(str, code_parts))
     loop_parts = map(lambda (start, end): prgm.loop(program[start:end]),loop_spans)
-    print("loop_spans:", loop_spans)
-    print("loop_parts:",map(str, loop_parts))
+    #print("loop_spans:", loop_spans)
+    #print("loop_parts:",map(str, loop_parts))
     return prgm.program(reduce(op.add, it.izip_longest(code_parts, loop_parts, fillvalue=''))) 
 
-print(str(compile('+++--->>>.....<')))
-print(str(compile('+++---[>.<]...<')))
-print(str(compile('+++[-+.][>+.]>....<')))
+def test():
+    testinput = [ 
+        '+++--->>>.....<',
+        '+++---[>.<]...<',
+        '+++[-+.][>+.]>....<',
+        '[-+.][>+.]>....<',
+        '+++[-+.][>+.]',
+        '+++[-+[++].][>+.]',
+        '[[[[[[[[++]]]]]]]]',
+        '[>[>[>[>[>[>[>[++]]]]]]]]'
+    ]
+
+    print("test: {status}".format(status=("Fail","OK")[all(map(lambda program:program==str(compile(program)), testinput))]))
 
