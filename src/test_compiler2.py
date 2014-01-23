432de4c6e08c64dc5a331af1af69f7fa2a1c079c
Jim Holmström <jim.holmstroem@gmail.com>
2014-01-23 06:50:20 +0100

    TST for compiler2

---
+++ src/test_compiler2.py
@@ -0,0 +1,36 @@
+from __future__ import print_function, division
+
+from functools import partial
+from nose.tools import assert_list_equal
+
+from compiler2 import compile, Code
+
+lift_str = partial(map, str)
+
+
+def test_str_identity_holds_after_compile():
+    test_input = map(Code, [
+        '+++--->>>.....<',
+        '+++---[>.<]...<',
+        '+++[-+.][>+.]>....<',
+        '[-+.][>+.]>....<',
+        '+++[-+.][>+.]',
+        '+++[-+[++].][>+.]',
+        '[[[[[[[[++]]]]]]]]',
+        '[>[>[>[>[>[>[>[++]]]]]]]]'
+    ])
+
+    assert_list_equal(
+        lift_str(map(compile, test_input)),
+        lift_str(test_input),
+    )
+    print(
+        "test: {status}".format(
+            status=("Fail", "OK")[
+                all(map(
+                    lambda code: str(code) == str(compile(code)),
+                    test_input
+                ))
+            ]
+        )
+    )
