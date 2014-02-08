37eaab6e83c702ad29c8b725fba73a9370e3ecd9
Jim Holmström <jim.holmstroem@gmail.com>
2014-02-08 17:34:38 +0100

    2nd argument to run_example_code specifies how many tokens you want to fetch before exiting

--- src/run_example_code.py
+++ src/run_example_code.py
@@ -15,7 +15,8 @@ from interpreter import run
 
 example_name = sys.argv[1]
 
-n_tokens_returned = 32
+
+n_tokens_returned = None if len(sys.argv) < 3 else int(sys.argv[2])
 
 
 with open('../brainfuck_code/{}.bf'.format(example_name), 'r') as file_:
