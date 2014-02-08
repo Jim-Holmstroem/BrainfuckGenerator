29e7af1c5b2b2af4a8a39ccbdfae17f085c42014
Jim Holmström <jim.holmstroem@gmail.com>
2014-02-08 07:34:56 +0100

    Unbuffured example code runner

--- src/run_example_code.py
+++ src/run_example_code.py
@@ -1,18 +1,24 @@
-#!/usr/bin/python
+#!/usr/bin/python -u
 from __future__ import print_function, division
+
+"""
+"""
 
 from itertools import chain
 from functools import partial
+
 import sys
+import os
 
 from interpreter import run
+
 
 example_name = sys.argv[1]
 
 with open('../brainfuck_code/{}.bf'.format(example_name), 'r') as file_:
-    program = ''.join(chain.from_iterable(file_))
-    map(
-        partial(print, end=''),
-        run(program)
-    )
-
+    with os.fdopen(sys.stdout.fileno(), 'w', 0) as stdout_unbuffered:
+        program = ''.join(chain.from_iterable(file_))
+        map(
+            stdout_unbuffered.write,
+            run(program)
+        )
