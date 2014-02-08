8adb42b7de12d9bbfcc7305382ce5ca8aac674f0
Jim Holmström <jim.holmstroem@gmail.com>
2014-02-08 17:23:13 +0100

    Removed the programatically nonbuffured stdout since it had issues with return what went wrong

--- src/run_example_code.py
+++ src/run_example_code.py
@@ -4,7 +4,7 @@ from __future__ import print_function, division
 """
 """
 
-from itertools import chain
+from itertools import chain, islice
 from functools import partial
 
 import sys
@@ -15,10 +15,12 @@ from interpreter import run
 
 example_name = sys.argv[1]
 
+n_tokens_returned = 32
+
+
 with open('../brainfuck_code/{}.bf'.format(example_name), 'r') as file_:
-    with os.fdopen(sys.stdout.fileno(), 'w', 0) as stdout_unbuffered:
-        program = ''.join(chain.from_iterable(file_))
-        map(
-            stdout_unbuffered.write,
-            run(program)
-        )
+    program = ''.join(chain.from_iterable(file_))
+    map(
+        partial(print, end=''),
+        islice(run(program), n_tokens_returned)
+    )
